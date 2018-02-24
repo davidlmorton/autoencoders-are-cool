e1c2c5bb650831325e039a6f12a7fa64998cdbea
David Morton <davidlmorton@gmail.com>
2018-02-23 22:04:21 -0600

    Extract DeconvolutionalLayer and update notebook

--- pytorch/ml/models/simple_convolutional_autoencoder.py
+++ pytorch/ml/models/simple_convolutional_autoencoder.py
@@ -23,6 +23,28 @@ class ConvolutionalLayer(nn.Module):
         return x
 
 
+class DeconvolutionalLayer(nn.Module):
+    def __init__(self, *, in_channels, out_channels,
+            stride=2, kernel_size=3, padding=1,
+            output_padding=1, bias=False, activation=nn.ReLU()):
+        super().__init__()
+        self.bn = nn.BatchNorm2d(num_features=in_channels)
+        self.deconv = nn.ConvTranspose2d(in_channels=in_channels,
+                out_channels=out_channels,
+                stride=stride,
+                kernel_size=kernel_size,
+                padding=padding,
+                output_padding=output_padding,
+                bias=bias)
+        self.activation = activation
+
+    def forward(self, x_in):
+        x = self.bn(x_in)
+        x = self.deconv(x)
+        x = self.activation(x)
+        return x
+
+
 class SimpleConvolutionalAutoencoder(nn.Module):
     def __init__(self, image_channels, conv_channels, conv_bias=False):
         super().__init__()
@@ -42,35 +64,23 @@ class SimpleConvolutionalAutoencoder(nn.Module):
                 padding=2,
                 bias=conv_bias)
 
-        self.bn4 = nn.BatchNorm2d(num_features=conv_channels[2])
-        self.deconv1 = nn.ConvTranspose2d(in_channels=conv_channels[2],
+        self.deconv1 = DeconvolutionalLayer(
+                in_channels=conv_channels[2],
                 out_channels=conv_channels[1],
-                stride=2,
-                kernel_size=3,
                 padding=2,
                 output_padding=0,
                 bias=conv_bias)
 
-        self.bn5 = nn.BatchNorm2d(num_features=conv_channels[1])
-        self.deconv2 = nn.ConvTranspose2d(in_channels=conv_channels[1],
+        self.deconv2 = DeconvolutionalLayer(
+                in_channels=conv_channels[1],
                 out_channels=conv_channels[0],
-                stride=2,
-                kernel_size=3,
-                padding=1,
-                output_padding=1,
                 bias=conv_bias)
 
-        self.bn6 = nn.BatchNorm2d(num_features=conv_channels[0])
-        self.deconv3 = nn.ConvTranspose2d(in_channels=conv_channels[0],
+        self.deconv3 = DeconvolutionalLayer(
+                activation=nn.Sigmoid(),
+                in_channels=conv_channels[0],
                 out_channels=image_channels,
-                stride=2,
-                kernel_size=3,
-                padding=1,
-                output_padding=1,
                 bias=conv_bias)
-
-        self.relu = nn.ReLU()
-        self.sigmoid = nn.Sigmoid()
 
     def encode(self, x_in, **kwargs):
         x = self.conv1(x_in)
@@ -79,17 +89,9 @@ class SimpleConvolutionalAutoencoder(nn.Module):
         return x_latent
 
     def decode(self, x_latent):
-        x = self.bn4(x_latent)
-        x = self.deconv1(x)
-        x = self.relu(x)
-
-        x = self.bn5(x)
+        x = self.deconv1(x_latent)
         x = self.deconv2(x)
-        x = self.relu(x)
-
-        x = self.bn6(x)
-        x = self.deconv3(x)
-        x_out = self.sigmoid(x)
+        x_out = self.deconv3(x)
         return x_out
 
     def forward(self, x_in, **kwargs):
