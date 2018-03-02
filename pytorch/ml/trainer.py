ee88ca398e8872119d7ad118b40d9c89707852bc
David Morton <davidlmorton@gmail.com>
2018-03-01 22:21:41 -0600

    Add method to calculate classification accuracy

--- pytorch/ml/trainer.py
+++ pytorch/ml/trainer.py
@@ -219,3 +219,12 @@ class Trainer:
                         c=labels.numpy(), alpha=0.4)
         plt.colorbar()
         return fig
+
+    def get_classification_accuracy(self):
+        num_correct = 0
+        for data, labels in self.test_data_loader:
+            out_dict = self._run_model(data, labels, train=False)
+            y_out = np.argmax(out_dict['y_out'].cpu().data.numpy(), axis=1)
+            y_in = out_dict['y_in'].cpu().data.numpy()
+            num_correct += (y_out - y_in == 0).sum()
+        return num_correct / len(self.test_data_loader.dataset)
